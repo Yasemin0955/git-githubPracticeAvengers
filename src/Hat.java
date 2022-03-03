56c044ae8438d470c4db8ae2c6677003de614326
Yasemin0955 <ymacit0955@gmail.com>
2022-03-03 19:25:55 +0000

    second commit at his branch

--- src/Hat.java
+++ src/Hat.java
@@ -1,5 +1,7 @@
 public class Hat {
     public static void main(String[] args) {
         //first commit in this branch
+        //second commit in this branch
+        System.out.println("second line of code");
     }
 }
